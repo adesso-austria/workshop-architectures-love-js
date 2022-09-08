c5acb42eb8b3eb632e8a36e0e3126f1cca18f9e3
Katja Potensky <katja.potensky@adesso.at>
2022-09-08 13:45:14 +0200

    feat(backend): add content to todo model

--- backend/src/test-data/todo.ts
+++ backend/src/test-data/todo.ts
@@ -4,6 +4,7 @@ import * as Domain from "../domain";
 export const buyIcecream: Domain.Todo.Todo = {
   id: Crypto.randomUUID(),
   title: "Buy Ice-Cream",
+  content: "buy some ice cream",
 };
 
 export const all = [buyIcecream];
