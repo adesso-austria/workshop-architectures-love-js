08552bf3e500f2890a001ed8ef267f64aa81481f
Katja Potensky <katja.potensky@adesso.at>
2022-09-08 15:31:34 +0200

    feat(backend): basic redis impl

---
+++ backend/src/repository/redis.e2e.ts
@@ -0,0 +1,64 @@
+import { describe, it, expect } from "@jest/globals";
+import { taskEither } from "fp-ts";
+import { pipe } from "fp-ts/lib/function";
+import { ignore, throwException } from "utils";
+import { connect } from "./redis";
+
+const connectClean = (url: string) =>
+  pipe(
+    connect(url, 1),
+    taskEither.chain((client) =>
+      pipe(
+        client.flush(),
+        taskEither.map(() => client)
+      )
+    )
+  );
+
+describe("redis", () => {
+  const defaultUrl = "redis://localhost:6379";
+
+  it(
+    "should return left if invalid connection url is given",
+    pipe(
+      connect("fuytnwrt"),
+      taskEither.match(ignore, () => throwException("expected a left"))
+    )
+  );
+
+  it(
+    "should return a right if a valid url is given",
+    pipe(
+      connect(defaultUrl),
+      taskEither.match(() => throwException("expected a right"), ignore)
+    )
+  );
+
+  describe("getLastEventId", () => {
+    it(
+      "should return the last event id",
+      pipe(
+        connectClean(defaultUrl),
+        taskEither.chain((client) =>
+          pipe(
+            client.emit({ foo: "bar" }),
+            taskEither.chain(() => client.getLastEventId())
+          )
+        ),
+        taskEither.match(throwException, (id) => expect(id).toBeDefined())
+      )
+    );
+
+    it(
+      "should return undefined if no events exist",
+      pipe(
+        connectClean(defaultUrl),
+        taskEither.chain((client) => client.getLastEventId()),
+        taskEither.match(
+          () => throwException("expected a right"),
+          (id) => expect(id).toBeUndefined()
+        )
+      )
+    );
+  });
+});
---
+++ backend/src/repository/redis.ts
@@ -0,0 +1,101 @@
+import { ioEither, taskEither } from "fp-ts";
+import { pipe } from "fp-ts/lib/function";
+import * as Redis from "redis";
+// eslint-disable-next-line import/no-internal-modules
+import { RedisFlushModes } from "@redis/client/dist/lib/commands/FLUSHALL";
+import { ignore } from "utils";
+
+type RedisClient = ReturnType<typeof Redis["createClient"]>;
+type RedisMessage = { id: string; message: Record<string, string | Buffer> };
+
+export type Client = {
+  emit: (
+    message: RedisMessage["message"]
+  ) => taskEither.TaskEither<string, void>;
+  /**
+   * flushes the whole db, won't work in production dbs.
+   * Only exists for integration tests
+   */
+  flush: () => taskEither.TaskEither<string, void>;
+  getLastEventId: () => taskEither.TaskEither<string, string | undefined>;
+  getEventsSince: (
+    id: string | undefined
+  ) => taskEither.TaskEither<string, RedisMessage[]>;
+};
+
+const emit =
+  (client: RedisClient): Client["emit"] =>
+  (message) =>
+    taskEither.tryCatch(
+      () => client.XADD("events", "*", message).then(ignore),
+      () => "could not emit message"
+    );
+
+const flush =
+  (client: RedisClient): Client["flush"] =>
+  () =>
+    taskEither.tryCatch(
+      () => client.FLUSHDB(RedisFlushModes.ASYNC).then(ignore),
+      () => "could not flush db"
+    );
+
+const getLastEventId =
+  (client: RedisClient): Client["getLastEventId"] =>
+  () =>
+    pipe(
+      taskEither.tryCatch(
+        () =>
+          client
+            .XINFO_STREAM("events")
+            .then((info) => info.lastEntry?.id)
+            .catch((error: Redis.ErrorReply) =>
+              error.message === "ERR no such key"
+                ? undefined
+                : Promise.reject(error)
+            ),
+        (reason) => (reason as Redis.ErrorReply).message
+      )
+    );
+
+const getEventsSince =
+  (client: RedisClient): Client["getEventsSince"] =>
+  (since) =>
+    pipe(
+      taskEither.tryCatch(
+        () => client.XRANGE("events", since ?? "-", "+"),
+        () => "could not read events since"
+      )
+    );
+
+export const connect = (
+  url = process.env["REDIS_URL"],
+  db = 0
+): taskEither.TaskEither<string, Client> => {
+  if (url == null) {
+    return taskEither.left("need to have a url to know where to connect to");
+  }
+  return pipe(
+    ioEither.tryCatch(
+      () => Redis.createClient({ url, database: db }),
+      () => "could not create db"
+    ),
+    taskEither.fromIOEither,
+    taskEither.chain((client) =>
+      taskEither.tryCatch(
+        () => client.connect().then(() => client),
+        () => "could not connect to client"
+      )
+    ),
+    taskEither.map(
+      (client): Client => ({
+        emit: emit(client),
+        flush:
+          db == 0
+            ? () => taskEither.left("refusing to delete events in prod db")
+            : flush(client),
+        getLastEventId: getLastEventId(client),
+        getEventsSince: getEventsSince(client),
+      })
+    )
+  );
+};
