56e8e2913a1eafd8701dce983d106c0745526e0c
Katja Potensky <katja.potensky@adesso.at>
2022-09-09 18:19:29 +0200

    refactor(backend/redis): factor out each function of client

--- backend/src/repository/redis.ts
+++ backend/src/repository/redis.ts
@@ -5,6 +5,7 @@ import * as Redis from "redis";
 import { RedisFlushModes } from "@redis/client/dist/lib/commands/FLUSHALL";
 import { ignore } from "utils";
 
+type RedisClient = ReturnType<typeof Redis["createClient"]>;
 type MessageContent = Record<string, string | Buffer>;
 type RedisMessage = { id: string; message: MessageContent };
 
@@ -21,6 +22,41 @@ export type ConnectOptions = {
   url?: string;
   db?: number;
 };
+
+const disconnect = (client: RedisClient) =>
+  taskEither.tryCatch(
+    () => client.disconnect(),
+    (reason) => reason as string,
+  );
+
+const addEvent = (client: RedisClient, message: MessageContent) =>
+  taskEither.tryCatch(
+    () => client.XADD("events", "*", message),
+    (reason) => reason as string,
+  );
+
+const getEvents = (client: RedisClient, since: option.Option<string>) =>
+  taskEither.tryCatch(
+    () =>
+      client.XRANGE(
+        "events",
+        pipe(
+          since,
+          option.map((id) => `(${id}`),
+          option.getOrElse(() => "-"),
+        ),
+        "+",
+      ),
+    (reason) => reason as string,
+  );
+
+const flush = (client: RedisClient, db: number) =>
+  db === 0
+    ? taskEither.left("refusing to drop prod db")
+    : taskEither.tryCatch(
+        () => client.flushDb(RedisFlushModes.ASYNC).then(ignore),
+        (reason) => reason as string,
+      );
 
 export const connect = ({
   url = process.env["REDIS_URL"],
@@ -41,40 +77,12 @@ export const connect = ({
         () => "could not connect to client",
       ),
     ),
-    // TODO: pull each function out
     taskEither.map(
       (client): Client => ({
-        disconnect: () =>
-          taskEither.tryCatch(
-            () => client.disconnect(),
-            (reason) => reason as string,
-          ),
-        addEvent: (message) =>
-          taskEither.tryCatch(
-            () => client.XADD("events", "*", message),
-            (reason) => reason as string,
-          ),
-        getEvents: (since) =>
-          taskEither.tryCatch(
-            () =>
-              client.XRANGE(
-                "events",
-                pipe(
-                  since,
-                  option.map((id) => `(${id}`),
-                  option.getOrElse(() => "-"),
-                ),
-                "+",
-              ),
-            (reason) => reason as string,
-          ),
-        flush: () =>
-          db === 0
-            ? taskEither.left("refusing to drop prod db")
-            : taskEither.tryCatch(
-                () => client.flushDb(RedisFlushModes.ASYNC).then(ignore),
-                (reason) => reason as string,
-              ),
+        disconnect: () => disconnect(client),
+        addEvent: (message) => addEvent(client, message),
+        getEvents: (since) => getEvents(client, since),
+        flush: () => flush(client, db),
       }),
     ),
   );
