02e2281016fe4589c38ae9159d7e0a2c86c81f57
Katja Potensky <katja.potensky@adesso.at>
2022-09-09 19:51:54 +0200

    feat(backend/db): raise test coverage

--- backend/src/repository/db.e2e.ts
+++ backend/src/repository/db.e2e.ts
@@ -1,15 +1,16 @@
 import { describe, it } from "@jest/globals";
 import { task, taskEither } from "fp-ts";
 import { flow, pipe } from "fp-ts/lib/function";
-import { ignore, throwException } from "utils";
+import { DeepPartial, ignore, throwException } from "utils";
+import * as TestUtils from "../test-utils";
 import * as Db from "./db";
 
 const withDb = (
   fn: (connect: taskEither.TaskEither<string, Db.Db>) => task.Task<void>,
-  options: Db.ConnectOptions,
+  options: DeepPartial<Db.ConnectOptions> = {},
 ) =>
   pipe(
-    Db.connect(options),
+    Db.connect(TestUtils.Repository.createConnectOptions({ db: options }).db),
     task.chainFirst(flow(taskEither.fromEither, fn)),
     taskEither.chain((db) => db.disconnect()),
   );
@@ -20,10 +21,40 @@ describe("db", () => {
       "should return left if given an invalid mongo url",
       withDb(
         taskEither.match(ignore, () => throwException("expected a left")),
-        {
-          mongo: { url: "fuytnrt", namespace: "doesn't matter" },
-          redis: { url: "abc", namespace: "tnuyrst" },
-        },
+        { mongo: { url: "utyfnrst" } },
+      ),
+    );
+
+    it(
+      "should return left if given an invalid redis url",
+      withDb(
+        taskEither.match(ignore, () => throwException("expected a left")),
+        { redis: { url: "tunyfwnrst" } },
+      ),
+    );
+
+    it(
+      "should return left if given invalid urls for redis and mongo",
+      withDb(
+        taskEither.match(ignore, () => throwException("expected a left")),
+        { redis: { url: "fuynt" }, mongo: { url: "nfytr" } },
+      ),
+    );
+
+    it(
+      "should return right if given valid urls",
+      withDb(taskEither.match(throwException, ignore)),
+    );
+  });
+
+  describe("disconnect", () => {
+    it(
+      "should return right for valid connections",
+      withDb(
+        flow(
+          taskEither.chain((db) => db.disconnect()),
+          taskEither.match(throwException, ignore),
+        ),
       ),
     );
   });
