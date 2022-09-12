7ea226c05815d0b05cd6f65530248474e2ffffdf
Katja Potensky <katja.potensky@adesso.at>
2022-09-13 00:07:02 +0200

    fix(backend/tests): properly disconnect mongo after test

--- backend/src/repository/todo.e2e.ts
+++ backend/src/repository/todo.e2e.ts
@@ -1,43 +1,60 @@
 import * as Crypto from "crypto";
 import { describe, expect, it } from "@jest/globals";
-import { pipe } from "fp-ts/lib/function";
-import { option, taskEither } from "fp-ts";
+import { flow, pipe } from "fp-ts/lib/function";
+import { option, task, taskEither } from "fp-ts";
 import { throwException } from "utils";
-import * as Adapters from "../adapters";
+import { Mongo } from "../adapters";
 import * as TestData from "../test-data";
 import * as Todo from "./todo";
 
-const connect = pipe(
-  Adapters.Mongo.connect({
-    url: "mongodb://localhost:27017",
-    namespace: Crypto.randomUUID(),
-  }),
-  taskEither.map((mongo) => Todo.create({ mongo })),
-);
+const withRepo = (
+  fn: (
+    repoTask: taskEither.TaskEither<string, Todo.Repository>,
+  ) => task.Task<void>,
+  url = "mongodb://localhost:27017",
+) =>
+  pipe(
+    Mongo.connect({
+      url,
+      namespace: Crypto.randomUUID(),
+    }),
+    task.chainFirst(
+      flow(
+        taskEither.fromEither,
+        taskEither.map((mongo) => Todo.create({ mongo })),
+        fn,
+      ),
+    ),
+    taskEither.chain((mongo) => mongo.disconnect()),
+  );
 
 describe("todo", () => {
   describe("getTodo", () => {
     it(
       "should return right none if the todo could not be found",
-      pipe(
-        connect,
-        taskEither.chain((repo) => repo.getTodo("foo")),
-        taskEither.match(throwException, (result) =>
-          expect(result).toEqual(option.none),
+      withRepo(
+        flow(
+          taskEither.chain((repo) => repo.getTodo("foo")),
+          taskEither.match(throwException, (result) =>
+            expect(result).toEqual(option.none),
+          ),
         ),
       ),
     );
 
     it(
       "should return some todo that has been added before",
-      pipe(
-        connect,
-        taskEither.chainFirst((repo) =>
-          repo.addTodo(TestData.Todo.buyIcecream),
-        ),
-        taskEither.chain((repo) => repo.getTodo(TestData.Todo.buyIcecream.id)),
-        taskEither.match(throwException, (result) =>
-          expect(result).toEqual(option.some(TestData.Todo.buyIcecream)),
+      withRepo(
+        flow(
+          taskEither.chainFirst((repo) =>
+            repo.addTodo(TestData.Todo.buyIcecream),
+          ),
+          taskEither.chain((repo) =>
+            repo.getTodo(TestData.Todo.buyIcecream.id),
+          ),
+          taskEither.match(throwException, (result) =>
+            expect(result).toEqual(option.some(TestData.Todo.buyIcecream)),
+          ),
         ),
       ),
     );
