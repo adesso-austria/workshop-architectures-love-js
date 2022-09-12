f3a023d371c9e1170c63d902814e396bdf7c5c2c
Katja Potensky <katja.potensky@adesso.at>
2022-09-12 17:10:57 +0200

    feat(utils): added anyrecord type

--- utils/src/types.ts
+++ utils/src/types.ts
@@ -1,5 +1,5 @@
-export type DeepPartial<T extends Record<string | number | symbol, unknown>> = {
-  [k in keyof T]?: T[k] extends Record<string | number | symbol, unknown>
-    ? DeepPartial<T[k]>
-    : T[k];
+export type AnyRecord = Record<string | number | symbol, unknown>;
+
+export type DeepPartial<T extends AnyRecord> = {
+  [k in keyof T]?: T[k] extends AnyRecord ? DeepPartial<T[k]> : T[k];
 };
