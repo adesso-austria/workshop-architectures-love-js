dfc81cec0dc9f8d2663f9af212e5d6d27ac0ae27
Katja Potensky <katja.potensky@adesso.at>
2022-09-16 17:10:01 +0200

    feat(frontend/tests): basic setup

---
+++ frontend/src/test-utils/render.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@material-tailwind/react";
+import * as TestingLibrary from "@testing-library/react";
+import React from "react";
+
+export type RenderOptions = unknown;
+
+function TestBed({ children }: React.PropsWithChildren) {
+  return <ThemeProvider>{children}</ThemeProvider>;
+}
+
+export const render = (component: JSX.Element, options: RenderOptions = {}) => {
+  return TestingLibrary.render(<TestBed>{component}</TestBed>);
+};
