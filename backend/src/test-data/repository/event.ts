80db647947504efd02f36a7e590f843fa456a616
Katja Potensky <katja.potensky@adesso.at>
2022-09-22 18:55:51 +0200

    feat(backend/test): naive mock implementation of event repo

--- backend/src/test-data/repository/event.ts
+++ backend/src/test-data/repository/event.ts
@@ -1,19 +1,40 @@
+import * as Crypto from "crypto";
 import { taskEither } from "fp-ts";
 import { mergeDeepRight } from "ramda";
 import { DeepPartial } from "utils";
 import * as Rx from "rxjs";
+import * as Domain from "../../domain";
 import * as Repository from "../../repository";
 import { createMock } from "../utils";
 
 export const create = (
   overrides: DeepPartial<Repository.Event.Repository>,
 ): Repository.Event.Repository => {
+  const events$ = new Rx.Subject<Domain.Event.Event>();
+  const ackEvents = [] as Array<{ consumer: string; eventId: string }>;
+
   return mergeDeepRight(
     createMock<Repository.Event.Repository>({
-      createEventStream: () => Rx.of(),
+      addEvent: (domainEvent) => {
+        const event: Domain.Event.Event = {
+          id: Crypto.randomUUID(),
+          domainEvent,
+        };
+        events$.next(event);
+        return taskEither.right(event);
+      },
+      createEventStream: () => events$,
       getUnknownEvents: () => taskEither.right([]),
-      acknowledgeEvent: () => taskEither.right(undefined),
-      hasEventBeenAcknowledged: () => taskEither.right(false),
+      acknowledgeEvent: (consumer, eventId) => {
+        ackEvents.push({ consumer, eventId });
+        return taskEither.right(undefined);
+      },
+      hasEventBeenAcknowledged: (consumer, eventId) =>
+        taskEither.right(
+          ackEvents.some(
+            (event) => event.consumer === consumer && event.eventId === eventId,
+          ),
+        ),
     }),
     overrides,
   );
