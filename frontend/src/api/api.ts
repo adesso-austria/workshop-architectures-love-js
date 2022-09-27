7fcc99691fc488dc71527201e98767547e4aa021
Katja Potensky <katja.potensky@adesso.at>
2022-09-27 17:25:12 +0200

    feat(frontend/api): added updateTodo

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,5 +1,6 @@
 import { option, taskEither } from "fp-ts";
 import { pipe } from "fp-ts/lib/function";
+import { match } from "ts-pattern";
 import { ignore } from "utils";
 import * as Domain from "../domain";
 import { Fetcher } from "./fetcher";
@@ -11,6 +12,7 @@ export type Api = {
   ) => taskEither.TaskEither<string, Domain.Todo.Todo>;
   fetchTodos: () => taskEither.TaskEither<string, Domain.Todo.Todo[]>;
   deleteTodo: (id: string) => taskEither.TaskEither<string, void>;
+  updateTodo: (todo: Domain.Todo.Todo) => taskEither.TaskEither<string, void>;
 };
 
 export const create = (fetcher: Fetcher): Api => ({
@@ -33,4 +35,26 @@ export const create = (fetcher: Fetcher): Api => ({
     ),
   deleteTodo: (id) =>
     pipe(fetcher.deleteTodo({ query: { id } }), taskEither.map(ignore)),
+  updateTodo: (todo) =>
+    pipe(
+      fetcher.putTodo({
+        body: {
+          id: todo.id,
+          title: todo.title,
+          content: pipe(
+            todo.content,
+            option.getOrElse(() => ""),
+          ),
+          isDone: todo.isDone,
+        },
+      }),
+      taskEither.chain((res) =>
+        match(res.status)
+          .with(204, () => taskEither.right(undefined))
+          .with(400, () => taskEither.left("validation error"))
+          .with(404, () => taskEither.left("can't update non-existent todo"))
+          .with(500, () => taskEither.left("server error"))
+          .exhaustive(),
+      ),
+    ),
 });
