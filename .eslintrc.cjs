b9e5319c6e3db298f984167203215c781e8eded3
Katja Potensky <katja.potensky@adesso.at>
2022-10-01 18:05:27 +0200

    feat(task-01): finish task-01

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,54 @@
  */
 const frontendRestrictedPaths = [
   {
-    target: "these/files/can't/import/from",
-    from: "these/files/can't/be/imported",
-    message: "the reason why target can't import from",
+    target: "frontend/src/components/**/*",
+    from: "node_modules/contracts",
+    message: "application must not use contracts directly",
+  },
+  {
+    target: "frontend/src/components/**/*",
+    from: "frontend/src/api/**/*",
+    message: "application should be store driven",
+  },
+  {
+    target: "frontend/src/store/**/*",
+    from: "frontend/src/components/**/*",
+    message: "store is driving application, not the other way around",
+  },
+  {
+    target: "frontend/src/store/**/*",
+    from: "node_modules/contracts",
+    message: "store must not use contracts directly",
+  },
+  {
+    target: "frontend/src/api/**/*",
+    from: "frontend/src/store/**/*",
+    message: "store depends on api, not the other way around",
+  },
+  {
+    target: "frontend/src/api/**/*",
+    from: "frontend/src/components/**/*",
+    message: "api should never get in direct contact with application layer",
+  },
+  {
+    target: "frontend/src/domain/**/*",
+    from: "node_modules/contracts",
+    message: "domain layer must be pure",
+  },
+  {
+    target: "frontend/src/domain/**/*",
+    from: "frontend/src/components/**/*",
+    message: "domain layer must be pure",
+  },
+  {
+    target: "frontend/src/domain/**/*",
+    from: "frontend/src/api/**/*",
+    message: "domain layer must be pure",
+  },
+  {
+    target: "frontend/src/domain/**/*",
+    from: "frontend/src/store/**/*",
+    message: "domain layer must be pure",
   },
 ];
 
@@ -13,6 +58,100 @@ const frontendRestrictedPaths = [
  * defines which layer can import which backend
  */
 const backendRestrictedPaths = [
+  {
+    target: "backend/src/application/**/*",
+    from: "node_modules/contracts",
+    message:
+      "application must not use contracts directly, use boundary instead",
+  },
+  {
+    target: "backend/src/application/**/*",
+    from: "backend/src/boundary/**/*",
+    message:
+      "boundary is edge of system, application is orchestrating internals",
+  },
+  {
+    target: "backend/src/application/**/*",
+    from: "backend/src/adapters/**/*",
+    message: "application should exclusively use repositories for persistence",
+  },
+  {
+    target: "backend/src/repository/**/*",
+    from: "backend/src/application/**/*",
+    message: "application is driving repository, not other way around",
+  },
+  {
+    target: "backend/src/repository/**/*",
+    from: "backend/src/boundary/**/*",
+    message: "repository must not get in contact with outside world",
+  },
+  {
+    target: "backend/src/repository/**/*",
+    from: "node_modules/contracts",
+    message: "repository should never use contracts, only domain",
+  },
+  {
+    target: "backend/src/boundary/**/*",
+    from: "backend/src/repository/**/*",
+    message: "boundary should never get in contact with persistence layer",
+  },
+  {
+    target: "backend/src/boundary/**/*",
+    from: "backend/src/adapters/**/*",
+    message: "boundary should never get in contact with persistence layer",
+  },
+  {
+    target: "backend/src/adapters/**/*",
+    from: "backend/src/repository/**/*",
+    message: "repositories use adapters, not the other way around",
+  },
+  {
+    target: "backend/src/adapters/**/*",
+    from: "backend/src/application/**/*",
+    message:
+      "low level technical implementations must not rely on orchestration logic",
+  },
+  {
+    target: "backend/src/adapters/**/*",
+    from: "backend/src/domain/**/*",
+    message: "adapters should be generic enough to not know about domain",
+  },
+  {
+    target: "backend/src/adapters/**/*",
+    from: "backend/src/boundary/**/*",
+    message:
+      "adapters are not supposed to interact with outside world on their own",
+  },
+  {
+    target: "backend/src/adapters/**/*",
+    from: "node_modules/contracts",
+    message: "adapters should not even know about contracts",
+  },
+  {
+    target: "backend/src/domain/**/*",
+    from: "node_modules/contracts",
+    message: "domain layer must be pure",
+  },
+  {
+    target: "backend/src/domain/**/*",
+    from: "backend/src/application/**/*",
+    message: "domain layer must be pure",
+  },
+  {
+    target: "backend/src/domain/**/*",
+    from: "backend/src/boundary/**/*",
+    message: "domain layer must be pure",
+  },
+  {
+    target: "backend/src/domain/**/*",
+    from: "backend/src/repository/**/*",
+    message: "domain layer must be pure",
+  },
+  {
+    target: "backend/src/domain/**/*",
+    from: "backend/src/adapters/**/*",
+    message: "domain layer must be pure",
+  },
 ];
 
 /**
--- frontend/src/components/overview.tsx
+++ frontend/src/components/overview.tsx
@@ -1,22 +1,12 @@
-import { either } from "fp-ts";
 import React from "react";
-import { ignore } from "utils";
-import * as Api from "../api";
-import * as Domain from "../domain";
+import * as Store from "../store";
 import { Todo } from "./todo";
 
 /**
  * renders an overview over the current todos
  */
 export const Overview = function TodoOverview() {
-  const [todos, setTodos] = React.useState([] as Domain.Todo.Todo[]);
-
-  const api = Api.useApi();
-
-  React.useEffect(() => {
-    const fetchTodos = api.fetchTodos();
-    fetchTodos().then(either.match(ignore, (todos) => setTodos(todos)));
-  }, []);
+  const { todos } = Store.Todo.useTodos();
 
   return (
     <>
