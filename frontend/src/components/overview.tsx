9ee5cace9db91d7bc8cb655da7bbbd1724a68e27
Katja Potensky <katja.potensky@adesso.at>
2022-10-01 18:04:31 +0200

    feat(task-01): start task-01

--- frontend/src/components/overview.tsx
+++ frontend/src/components/overview.tsx
@@ -1,12 +1,22 @@
+import { either } from "fp-ts";
 import React from "react";
-import * as Store from "../store";
+import { ignore } from "utils";
+import * as Api from "../api";
+import * as Domain from "../domain";
 import { Todo } from "./todo";
 
 /**
  * renders an overview over the current todos
  */
 export const Overview = function TodoOverview() {
-  const { todos } = Store.Todo.useTodos();
+  const [todos, setTodos] = React.useState([] as Domain.Todo.Todo[]);
+
+  const api = Api.useApi();
+
+  React.useEffect(() => {
+    const fetchTodos = api.fetchTodos();
+    fetchTodos().then(either.match(ignore, (todos) => setTodos(todos)));
+  }, []);
 
   return (
     <>
